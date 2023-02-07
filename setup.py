f09fd600d912ee42fce6570d95c4282cec6e8990
Atul <anamikagolu1508@gmail.com>
2023-02-07 05:56:06 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+def get_requirements():
+    pass
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="Atul Mishra",
+    author_mail="anamikagolu1508@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
