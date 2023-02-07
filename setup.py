692cb29a566bf4fa8933e34edd8153eb3bbeb7bc
Atul <anamikagolu1508@gmail.com>
2023-02-07 07:37:35 +0530

    update files and folders

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import find_packages,setup
 from typing import List
 
 REQUIREMENTS_FILE_NAME= "requirements.txt"
+HYPHEN_E_DOT = "-e ."
 
 def get_requirements() -> list[str]:
     with open (REQUIREMENTS_FILE_NAME) as requirements_file:
         requirements_list = requirements_file.readlines()
-    requirements_list=[requirements_name.replace("\n", "")for requirements_name in ]
-    
+    requirements_list=[requirements_name.replace("\n", "")for requirements_name in requirement_list]
+
     if HYPHEN_E_DOT in requirements_list:
         requirements_list.remove(HYPHEN_E_DOT)
     return requirements_list
