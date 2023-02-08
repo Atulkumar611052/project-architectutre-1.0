ac3b5670157c80493f8dd721b3641fc903d7b6da
Atul <anamikagolu1508@gmail.com>
2023-02-08 07:21:53 +0530

    update code in github

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
-import pymongo
+import sys,os
+from sensor.logger import logging
+from sensor.exception import SensorException
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
-# Database Name
-dataBase = client["neurolabDB"]
+def test_logger_and_exception():
+     try:
+          logging.info("Starting the test_logger_and_exception")
+          result = 3/0
+          print(result)
+          logging.info("Stopping the test_logger_and_exception")          
+     except Exception as e:
+          logging.debug("Stopping the test_logger_and_exception")
+          raise SensorException(e,sys)
 
-# Collection  Name
-collection = dataBase['Products']
 
-# Sample data
-d = {'companyName': 'iNeuron',
-     'product': 'Affordable AI',
-     'courseOffered': 'Machine Learning with Deployment'}
+if __name__=="__main__":
+     try:
+          test_logger_and_exception()
 
-# Insert above records in the collection
-rec = collection.insert_one(d)
-
-# Lets Verify all the record at once present in the record with all the fields
-all_record = collection.find()
-
-# Printing all records present in the collection
-for idx, record in enumerate(all_record):
-     print(f"{idx}: {record}")
+     except Exception as e:
+          print(e)
--- sensor/logger.py
+++ sensor/logger.py
@@ -17,7 +17,7 @@ LOG_FILE_PATH= os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
 logging.basicConfig(
     filename= LOG_FILE_PATH,
     format="[%(asctime)s] %(lineno)d %(name)s -%(levelname)s -%(message)s",
-    level=logging.INFO,
+    level=logging.DEBUG,
 
 
 )
