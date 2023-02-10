be7cb1114c9047ee89fa4dc138699b757b73e5b8
Atul <anamikagolu1508@gmail.com>
2023-02-10 07:49:18 +0530

    update code in github

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from sensor.utils import get_collection_as_dataframe
 from sensor.entity.config_entity import DataIngestionConfig
 from sensor.entity import config_entity
 from datetime import datetime
+from sensor.components import data_ingestion
 def test_logger_and_exception():
      try:
           logging.info("Starting the test_logger_and_exception")
@@ -22,5 +23,7 @@ if __name__=="__main__":
           training_pipeline_config= config_entity.TrainingPipelineConfig()
           data_ingestion_config= DataIngestionConfig(training_pipeline_config=training_pipeline_config)
           print(data_ingestion_config.to_dict())
+          data_ingestion=DataIngestion(data_ingestion_config=data_ingestion_config)
+          print(data_ingestion.initiate_data_ingestion())
      except Exception as e:
           print(e)
